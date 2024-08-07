bb004d9f3d508bf1caf8da0aeb42768bbf7ff542
alexwindelberg <alexwindelberg@gmail.com>
2024-08-07 13:06:56 -0400

    not found pages

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -2,8 +2,6 @@ export default function Docs ({ params }: { params: {
     slug: string[];
 }}) {
 
-    console.log('This is the slug ', params.slug);
-
     if(params.slug?.length === 2) {
         return <h1>Viewing docs for feature {params.slug[0]} and concept {params.slug[1]} </h1>
     }
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,4 +1,9 @@
+import { notFound } from 'next/navigation'
 
 export default function ReviewDetail({ params } : { params: { productId: string, reviewId: string } }) {
-    return <h1>This is {params.productId} and review { params.reviewId }</h1>
+
+    if(parseInt(params.reviewId) > 1000) {
+        notFound();
+    }
+    else return <h1>This is {params.productId} and review { params.reviewId }</h1>
 }
