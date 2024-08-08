0d15acd76c0ca1a9ebc599c5f36f502a2decd12e
alexwindelberg <alexwindelberg@gmail.com>
2024-08-08 14:17:46 -0400

    meta data - generate metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,6 @@
+export const metadata = {
+    title: "About Alex"
+}
 
 export default function About() {
     return <h1 className="text-3xl">About</h1>
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,6 +1,25 @@
+import { Metadata } from 'next'
 
+type Props = {
+    params: {
+        productId: string
+    }
+}
 
-export default function ProductDetails({ params }: { params: { productId: string} }) {
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+    
+    const title = await new Promise(resolve => {
+        setTimeout(() => {
+            resolve(`iPhone ${params.productId}`)
+        }, 100)
+    })
+    
+    return {
+        title : `Product ${title}`
+    }
+}
+
+export default function ProductDetails({ params }: Props) {
     return <>
     <h1>Product {params.productId}</h1>
     <h2>Product Name</h2>
