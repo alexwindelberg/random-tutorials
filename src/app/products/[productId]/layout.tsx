905a87c1f98423a3bc5079a8b76d6ce2088ba1bc
alexwindelberg <alexwindelberg@gmail.com>
2024-08-08 12:27:39 -0400

    Nested layouts

---
+++ src/app/products/[productId]/layout.tsx
@@ -0,0 +1,6 @@
+export default function ProductDetailsLayout({ children }: {children: React.ReactNode}) {
+    return (<>
+    {children}
+    <h2>This is the new layout for the products </h2>
+        </>)
+}
