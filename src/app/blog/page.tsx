bf6829128d0d9caa5bfef2d9492f7bc15ce424db
alexwindelberg <alexwindelberg@gmail.com>
2024-08-08 16:24:00 -0400

    dynamic meta data

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,8 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: 'Alex\'s Blog'
+}
 
 export default function Blog() {
     return <h1>My Blog</h1>
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,8 @@
+import { Metadata } from 'next';
+
+export const meta: Metadata = {
+    title: { absolute: 'Alex\'s absolute path' }
+}
 
 export default function Profile() {
     return <h1 className="text-3xl">Profile</h1>
