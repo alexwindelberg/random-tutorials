b84d49cefcebed3ea8580ba632004c18037e6704
alexwindelberg <alexwindelberg@gmail.com>
2024-08-12 10:01:00 -0400

    nested errors

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,20 @@
 import { notFound } from 'next/navigation'
 
+function getRandomInt(count: number) {
+    return Math.floor(Math.random() * count);
+}
+
 export default function ReviewDetail({ params } : { params: { productId: string, reviewId: string } }) {
+
+    const random = getRandomInt(2);
+
+    if(random === 1) {
+        throw new Error("Error loading review");
+    }
 
     if(parseInt(params.reviewId) > 1000) {
         notFound();
     }
+    
     else return <h1>This is {params.productId} and review { params.reviewId }</h1>
 }
